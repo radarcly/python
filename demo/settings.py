d25606906e5d20c279b19a1d7a4be2788ee371ac
radarcly <16302010001@fudan.edu.cn>
2017-12-05 23:14:44 +0800

    demo version1

---
+++ demo/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+        """store all the setting of the game"""
+        def __init__(self):
+            """init"""
+             #set screen
+            self.screen_width = 1200
+            self.screen_height = 800
+            self.bg_color = (230, 230, 230)
